fd087b4a691b820906da4a2ee1074ce49a8ad603
Akash <agampala@ufl.edu>
2023-08-29 12:10:39 -0400

    Create server and connect to db

--- back-end/app.js
+++ back-end/app.js
@@ -1,3 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+
+const app = express()
+
+const dbUri = 'mongodb+srv://akashgampala229:pldb123@cluster0.skh2hkp.mongodb.net/?retryWrites=true&w=majority'
+
+// Connecting to db
+mongoose.connect(dbUri);
+
+app.use(bodyParser.json())
+
+
 
