ffb26058751a8ee095c4f6dcd7d81fa52f5c959e
Akash <agampala@ufl.edu>
2023-08-29 12:19:33 -0400

    Route handling and error handling for invalid routes

--- back-end/app.js
+++ back-end/app.js
@@ -9,7 +9,31 @@ const dbUri = 'mongodb+srv://akashgampala229:pldb123@cluster0.skh2hkp.mongodb.ne
 // Connecting to db
 mongoose.connect(dbUri);
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
+// Importing routes
+const userRoutes = require('./api/routes/users');
+
+// Route handling
+
+app.use('/users', userRoutes)
+
+// Error handling
+
+app.use((req,res,next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
 
 
+app.use((error,req,res,next) => {
+    res.status(error.status || 500);
+    res.json({
+        error:{
+            message:error.message
+        }
+    });
+});
 
+module.exports = app;
