06c55d73301afa4274aea02ecc417b8219e10f47
Akash <agampala@ufl.edu>
2023-08-29 13:07:53 -0400

    Create delete route in user controller file

---
+++ back-end/api/controllers/user.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const User = require('../models/user');
+
+// Create a user using a post request - sign-up page
+exports.user_sign_up = (req,res) => {
+    User.find({email: req.body.email})
+        .then((users) => {
+            // Check if the email address has been used already
+            if (users.length >= 1) {
+                return res.status(409).json({
+                    message: "Email address already exists"
+                })
+            } else {
+                // Hash the password that was sent in the request body
+                bcrypt.hash(req.body.password, 10, (err, hash) =>{
+                    if (err) {
+                        return res.status(200).json({
+                            error: err
+                        });
+                    } else {
+                        // Create a new user using the information from the payload / request body
+                        const user = new User({
+                            _id: new mongoose.Types.ObjectId(),
+                            username: req.body.username,
+                            email: req.body.email,
+                            password: hash,
+                            createdAt: Date.now()
+                        });
+
+                        // Save the new user to the db
+                        user.save()
+                            .then((result) => {
+                                console.log(result);
+                                res.status(201).json({
+                                    message: "User created"
+                                });
+                            })
+                            .catch((err) => {
+                                console.log(err);
+                                res.status(500).json({
+                                    error: err
+                                });
+                            });
+                    }
+                })
+            }
+        })
+}
+
+// Delete a user
+exports.user_delete = (req,res) => {
+    User.remove({_id: req.params.userId})
+        .then((result) => {
+            res.status(200).json({
+                message: "User deleted"
+            });
+        })
+        .catch((err) => {
+            res.status(500).json({
+                error: err
+            });
+        });
+}
--- back-end/api/routes/users.js
+++ back-end/api/routes/users.js
@@ -10,51 +10,15 @@ const router = express.Router();
 
 const User = require('../models/user');
 
+// Require user controller
+const userController = require("../controllers/user");
+
+
 // Create a user using a post request - sign-up page
+router.post('/signup', userController.user_sign_up);
 
-router.post('/signup', (req,res) => {
-    User.find({email: req.body.email})
-        .then((users) => {
-            // Check if the email address has been used already
-            if (users.length >= 1) {
-                return res.status(409).json({
-                    message: "Email address already exists"
-                })
-            } else {
-                // Hash the password that was sent in the request body
-                bcrypt.hash(req.body.password, 10, (err, hash) =>{
-                    if (err) {
-                        return res.status(200).json({
-                            error: err
-                        });
-                    } else {
-                        // Create a new user using the information from the payload / request body
-                        const user = new User({
-                            _id: new mongoose.Types.ObjectId(),
-                            username: req.body.username,
-                            email: req.body.email,
-                            password: hash,
-                            createdAt: Date.now()
-                        });
+// Delete a user
+router.delete('/:userId', userController.user_delete);
 
-                        // Save the new user to the db
-                        user.save()
-                            .then((result) => {
-                                console.log(result);
-                                res.status(201).json({
-                                    message: "User created"
-                                });
-                            })
-                            .catch((err) => {
-                                console.log(err);
-                                res.status(500).json({
-                                    error: err
-                                });
-                            });
-                    }
-                })
-            }
-        })
-})
 
 module.exports = router
