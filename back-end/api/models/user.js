fd83afea88b8be191ec44050e20f7e929a8c56e9
Akash <agampala@ufl.edu>
2023-08-29 12:56:16 -0400

    Create post route for user sign-up

--- back-end/api/models/user.js
+++ back-end/api/models/user.js
@@ -1,11 +1,25 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now }
+    _id: mongoose.Schema.Types.ObjectId,
+    username: { 
+      type: String, 
+      required: true, 
+      unique: true 
+    },
+    email: {
+      type: String, 
+      required: true, 
+      unqiue: true
+    },
+    password: { 
+      type: String,
+      required: true 
+    },
+    createdAt: { 
+      type: Date, 
+      default: Date.now 
+    }
   });
 
 const User = mongoose.model('User', userSchema);
--- back-end/api/routes/users.js
+++ back-end/api/routes/users.js
@@ -1,1 +1,60 @@
 // route for /users
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const router = express.Router();
+
+// User - id, username, email, password, createdAt
+
+const User = require('../models/user');
+
+// Create a user using a post request - sign-up page
+
+router.post('/signup', (req,res) => {
+    User.find({email: req.body.email})
+        .then((users) => {
+            // Check if the email address has been used already
+            if (users.length >= 1) {
+                return res.status(409).json({
+                    message: "Email address already exists"
+                })
+            } else {
+                // Hash the password that was sent in the request body
+                bcrypt.hash(req.body.password, 10, (err, hash) =>{
+                    if (err) {
+                        return res.status(200).json({
+                            error: err
+                        });
+                    } else {
+                        // Create a new user using the information from the payload / request body
+                        const user = new User({
+                            _id: new mongoose.Types.ObjectId(),
+                            username: req.body.username,
+                            email: req.body.email,
+                            password: hash,
+                            createdAt: Date.now()
+                        });
+
+                        // Save the new user to the db
+                        user.save()
+                            .then((result) => {
+                                console.log(result);
+                                res.status(201).json({
+                                    message: "User created"
+                                });
+                            })
+                            .catch((err) => {
+                                console.log(err);
+                                res.status(500).json({
+                                    error: err
+                                });
+                            });
+                    }
+                })
+            }
+        })
+})
+
+module.exports = router
