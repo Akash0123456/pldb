674f9237210d1d3de558c23e960f2ba05c4719b4
Akash <agampala@ufl.edu>
2023-09-07 16:26:08 -0400

    Small changes

--- back-end/api/routes/games.js
+++ back-end/api/routes/games.js
@@ -4,7 +4,4 @@ const router = express.Router();
 // Require Game controller
 const gameController = require('../controllers/game');
 
-// Add a game to the db using a post request
-router.post('/add-game', )
-
 module.exports = router;
